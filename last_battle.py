79c1aa8069b6e79c12289f00772836c9f3e828e1
uranium__hexafluoride <139465358+Homo-sapiens-sapiens@users.noreply.github.com>
2026-07-05 20:35:05 +0500

    The game itself
    Start to develop

--- last_battle.py
+++ last_battle.py
@@ -12,7 +12,11 @@ fusers = {}
 
 class MyGame:
     def __init__(self, player1, player2):
-        global fusers
+        self.user1 = None
+        self.user2 = None
+        self.view1 = None
+        self.view2 = None
+    async def create(self, player1, player2):
         self.user1 = player1
         self.user2 = player2
         self.view1 = self.user1.view
@@ -21,24 +25,26 @@ class MyGame:
         self.view2.game = self
         self.user1.game = self
         self.user2.game = self
-        fusers.pop(self.user1.id)
-        fusers.pop(self.user2.id)
+        fusers.pop(self.user1.id, None)
+        fusers.pop(self.user2.id, None)
+        await self.view1.show_game()
+        await self.view2.show_game()
 
 class MyView(DesignerView):
-    def __init__(self, user: MyUser):
-        global fusers
+    def __init__(self, user):
         self.user = user
         self.game = None
-        self.container = None
+        self.menu = None
+        self.table = None
         super().__init__(timeout=30)
-        text1 = TextDisplay("LAST BATTLE")
-        text2 = TextDisplay("Survive by destroing the enemy")
+        text1 = TextDisplay("# LAST BATTLE")
+        text2 = TextDisplay("Main menu")
         thumbnail = Thumbnail(bot.user.display_avatar.url)
         section = Section(text1, text2, accessory=thumbnail)
         section.add_text("-# Good luck")
-        self.container = Container(section, color=Color.from_rgb(180, 180, 180))
+        self.menu = Container(section, color=Color.from_rgb(180, 180, 180))
         async def delete_callback(interaction: Interaction):
-            if self.user.id in fusers: fusers.pop(self.user.id)
+            fusers.pop(self.user.id, None)
             self.stop()
             await interaction.response.defer()
             await self.user.thread.delete()
@@ -46,18 +52,15 @@ class MyView(DesignerView):
             if self.user.id in fusers:
                 await interaction.response.send_message("You already are waiting for opponent",ephemeral=True)
                 return
-            elif len(fusers) == 0:
+            if len(fusers) == 0:
                 fusers[self.user.id] = self.user
-                self.container.add_item(TextDisplay("Waiting for the opponent"))
+                self.menu.add_item(TextDisplay("Waiting for the opponent"))
                 await interaction.response.edit_message(view=self)
                 return
-            else:
-                opponent = next(iter(fusers.values()))
-                fusers.pop(opponent.id)
-                game = MyGame(opponent, self.user)
-                self.container.add_item(TextDisplay(f"The game created: {opponent.name} vs {self.user.name}"))
-                await interaction.response.edit_message(view=self)
-                return
+            opponent = next(iter(fusers.values()))
+            game = MyGame(opponent, self.user)
+            await interaction.response.defer()
+            await game.create(opponent, self.user)
         delete_button = Button(label="Delete Thread", style=ButtonStyle.red, id=0)
         delete_button.callback = delete_callback
         play_button = Button(label="Start the game", style=ButtonStyle.green, id=0)
@@ -65,8 +68,16 @@ class MyView(DesignerView):
         row = ActionRow()
         row.add_item(delete_button)
         row.add_item(play_button)
-        self.container.add_item(row)
-        self.add_item(self.container)
+        self.menu.add_item(row)
+        self.add_item(self.menu)
+    
+    async def show_game(self):
+        self.clear_items()
+        self.table = Container(color=Color.from_rgb(180, 180, 180))
+        self.table.add_item(TextDisplay("# LAST BATTLE"))
+        self.table.add_item(TextDisplay("game interface"))
+        self.add_item(self.table)
+        await self.user.message.edit(view=self)
 
 class MyUser:
     def __init__(self):
@@ -75,6 +86,7 @@ class MyUser:
         self.name = None
         self.view = None
         self.game = None
+        self.message = None
     @classmethod
     async def create(cls, ctx: discord.ApplicationContext):
         global users
@@ -86,7 +98,7 @@ class MyUser:
         await self.thread.add_user(ctx.author)
         await self.thread.send(f"The new game thread for {self.name} was created")
         self.view = MyView(self)
-        await self.thread.send(view=self.view)
+        self.message = await self.thread.send(view=self.view)
         await ctx.send(f"{self.thread.mention} thread for {self.name} created")
         return self
 
